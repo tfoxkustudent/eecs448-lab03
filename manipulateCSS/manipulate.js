e5685f4f0375ba1e5e86098adcaafb9b6021d2f7
Tim Fox <tfoxku@gmail.com>
2018-09-19 22:16:02 -0500

    exercise 4

---
+++ manipulateCSS/manipulate.js
@@ -0,0 +1,14 @@
+function colors()
+{
+  let A =  document.getElementById('br');
+  let B = document.getElementById('bg');
+  let C =  document.getElementById('bb');
+  let D = document.getElementById('width');
+  let E =  document.getElementById('bgr');
+  let F = document.getElementById('bgg');
+  let G = document.getElementById('bgb');
+
+  let p = document.getElementById('p1');
+  p.style.backgroundColor = rgb(E,F,G);
+
+}
