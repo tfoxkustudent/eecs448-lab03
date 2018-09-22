6682c2e28064391d11082df54463bfd40b73ac0e
Tim Fox <tfoxku@gmail.com>
2018-09-21 20:18:45 -0500

    finished exercise 4

--- manipulateCSS/manipulate.js
+++ manipulateCSS/manipulate.js
@@ -1,14 +1,16 @@
 function colors()
 {
-  let A =  document.getElementById('br');
-  let B = document.getElementById('bg');
-  let C =  document.getElementById('bb');
-  let D = document.getElementById('width');
-  let E =  document.getElementById('bgr');
-  let F = document.getElementById('bgg');
-  let G = document.getElementById('bgb');
+  let A =  document.getElementById('br').value;
+  let B = document.getElementById('bg').value;
+  let C =  document.getElementById('bb').value;
+  let D = document.getElementById('width').value;
+  let E =  document.getElementById('bgr').value;
+  let F = document.getElementById('bgg').value;
+let G = document.getElementById('bgb').value;
+let p = document.getElementById("p");
 
-  let P = document.getElementById('p1');
-  P.style.backgroundColor = "rgb(E, F, G)";
-
+  p.style.borderStyle= "solid";
+  p.style.borderWidth=`${D}`;
+  p.style.borderColor=`rgb(${A}, ${B}, ${C})`;
+  p.style.backgroundColor=`rgb(${E}, ${F}, ${G})`;
 }
